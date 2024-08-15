99281a0c2ac557325b1eeaa91a58b5fb9f774aa5
zerpico <zerpico@yandex.ru>
2024-08-15 19:57:39 +0500

    fake dummy source

--- src/common/NtDummyVideoDevice.h
+++ src/common/NtDummyVideoDevice.h
@@ -1,12 +1,43 @@
 #pragma once
 
 #include "NtDeviceInterface.h"
+#include <string>
+
+struct DummyVideoDeviceParameters
+{
+    DummyVideoDeviceParameters(const int width = 640, const int height = 480, const double fps = 25.0, const char *title_text = NULL, const bool add_timer = false)
+        : m_devName("dummy_video"), m_width(width), m_height(height), m_fps(fps), m_title(title_text), m_add_timer(add_timer)
+    {
+    }
+
+    std::string m_devName;
+    int m_width;
+    int m_height;
+    double m_fps;
+    std::string m_title;
+    bool m_add_timer;
+};
 
 class NtDummyVideoDevice : public NtDeviceInterface
 {
-private:
-    /* data */
+
 public:
-    NtDummyVideoDevice(/* args */);
-    ~NtDummyVideoDevice();
+    static NtDummyVideoDevice *createNew(const DummyVideoDeviceParameters &params);
+    virtual ~NtDummyVideoDevice();
+    void close();
+
+protected:
+    NtDummyVideoDevice(const DummyVideoDeviceParameters &params);
+
+public:
+    virtual size_t read(char *buffer, size_t bufferSize) { return 0; }
+    virtual int getFd() { return -1; }
+    virtual unsigned long getBufferSize() { return 0; }
+
+    virtual int getWidth() { return m_params.m_width; }
+    virtual int getHeight() { return m_params.m_height; }
+    // virtual int getVideoFormat() { return m_device->getFormat(); }
+
+private:
+    DummyVideoDeviceParameters m_params;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,54 @@
 #include "WebServer.h"
 #include "NtRtspApp.h"
 
+// PAL dimensions
+static const unsigned kWidth = 720;
+static const unsigned kHeight = 576;
+
+typedef struct
+{
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} RGB;
+
 int main()
 {
+    const RGB BAR_COLOUR[8] =
+        {
+            {255, 255, 255}, // 100% White
+            {255, 255, 0},   // Yellow
+            {0, 255, 255},   // Cyan
+            {0, 255, 0},     // Green
+            {255, 0, 255},   // Magenta
+            {255, 0, 0},     // Red
+            {0, 0, 255},     // Blue
+            {0, 0, 0},       // Black
+        };
+
+    // Allocate frame buffer
+    size_t frameBytes = kWidth * kHeight * sizeof(RGB);
+    RGB *frame = new RGB[kWidth * kHeight];
+    unsigned columnWidth = kWidth / 8;
+
+    // Generate complete frame
+    for (unsigned y = 0; y < kHeight; y++)
+    {
+        for (unsigned x = 0; x < kWidth; x++)
+        {
+            unsigned col_idx = x / columnWidth;
+            frame[y * kWidth + x] = BAR_COLOUR[col_idx];
+        }
+    }
+
+    // Save as PPM
+    FILE *fout = fopen("ebu_bars.ppm", "wb");
+    fprintf(fout, "P6\n%u %u\n255\n", kWidth, kHeight);
+    fwrite(frame, frameBytes, 1, fout);
+    fclose(fout);
+
+    free(frame);
+
     auto rtsp = new NtRtspApp();
     auto web = new WebServer();
     auto device = new CameraDeviceSource();
