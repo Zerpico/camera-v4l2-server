77dff812d62c36f2845c785e35038ac12fa5634e
zerpico <zerpico@yandex.ru>
2024-08-16 20:13:49 +0500

    wr encoder

--- src/device/NtVideoEncoder.cpp
+++ src/device/NtVideoEncoder.cpp
@@ -40,90 +40,51 @@ NtVideoEncoder::NtVideoEncoder(/* args */)
     double fps = 25.0;
     AVPixelFormat format = AVPixelFormat::AV_PIX_FMT_YUV420P;
     int gop_size = fps;
+
+    int ret = 0;
+    const AVCodec *codec = NULL;
+
+    codec = avcodec_find_encoder_by_name(codec_name.c_str());
+
+    if (codec)
     {
-        int ret = 0;
-        const AVCodec *codec = NULL;
-
-        codec = avcodec_find_encoder_by_name(codec_name.c_str());
-
-        if (codec)
-        {
-            spdlog::error("Can't open codec by name '{0}'", codec_name);
-            return;
-        }
-
-        auto codec_ctx = std::shared_ptr<AVCodecContext>(avcodec_alloc_context3(codec), [](AVCodecContext *pi)
-                                                         { avcodec_free_context(&pi); });
-        if (codec_ctx)
-        {
-            spdlog::error("Can't allocate codec context");
-            return;
-        }
-
-        auto framerate = av_d2q(fps, 100);
-
-        codec_ctx->width = width;
-        codec_ctx->height = height;
-        codec_ctx->framerate = framerate;
-        codec_ctx->pix_fmt = format;
-        codec_ctx->time_base = av_inv_q(framerate); // Frames per second
-        if (gop_size > 0)
-            codec_ctx->gop_size = gop_size; // Intra frames per x P frames
-        // CodecCtx->bit_rate = config.BitRate;     // average bit_rate
-
-        if (codec->id == AVCodecID::AV_CODEC_ID_H264 || codec->id == AVCodecID::AV_CODEC_ID_HEVC ||
-            codec->id == AVCodecID::AV_CODEC_ID_MPEG4 || codec->id == AVCodecID::AV_CODEC_ID_MPEG2VIDEO)
-        {
-            codec_ctx->thread_count = 1;
-        }
-
-        if (codec->id == AVCodecID::AV_CODEC_ID_MPEG4 || codec->id == AVCodecID::AV_CODEC_ID_MPEG2VIDEO)
-        {
-            codec_ctx->max_b_frames = 1;
-        }
-
-        // добавляем словарь метаданные для кодировщика
-        if (config.Metadata != null)
-            fixed(AVDictionary **optPtr = &_encodingOpt)
-            {
-                foreach (var opt in config.Metadata)
-                {
-                    if (opt.Value is string a)
-                        av_dict_set(optPtr, opt.Key, a, 0);
-                    else if (opt.Value is int b)
-                        av_dict_set_int(optPtr, opt.Key, b, 0);
-                    else if (opt.Value is System.Text.Json.JsonElement j)
-                    {
-                        if (j.ValueKind == System.Text.Json.JsonValueKind.String)
-                            av_dict_set(optPtr, opt.Key, j.GetString(), 0);
-                        if (j.ValueKind == System.Text.Json.JsonValueKind.Number)
-                            av_dict_set_int(optPtr, opt.Key, j.GetInt32(), 0);
-                    }
-                }
-            }
-
-        // CodecCtx->flags &= ~AV_CODEC_FLAG_GLOBAL_HEADER;
-
-        // fixed(AVDictionary **optPtr = &_encodingOpt)
-        //     ret = avcodec_open2(CodecCtx, codec, optPtr);
-        // if (ret != 0)
-        //     return false;
-
-        // OutPacket = av_packet_alloc();
-
-        // // указываем что не все метаданные кодировщика использованы
-        // if (_encodingOpt != null)
-        // {
-        //     AVDictionaryEntry *dictEntry = null;
-        //     dictEntry = av_dict_get(_encodingOpt, "", dictEntry, AV_DICT_IGNORE_SUFFIX);
-        //     do
-        //     {
-        //         Log.Warn($ "Codec option not used: '{PtrHelpers.BytePtrToStringUTF8(dictEntry->key)}' = '{PtrHelpers.BytePtrToStringUTF8(dictEntry->value)}'");
-        //     } while ((dictEntry = av_dict_get(_encodingOpt, "", dictEntry, AV_DICT_IGNORE_SUFFIX)) != null);
-        // }
-
-        // return true;
+        spdlog::error("Can't open codec by name '{0}'", codec_name);
+        return;
     }
+
+    auto codec_ctx = std::shared_ptr<AVCodecContext>(avcodec_alloc_context3(codec), [](AVCodecContext *pi)
+                                                     { avcodec_free_context(&pi); });
+    if (codec_ctx)
+    {
+        spdlog::error("Can't allocate codec context");
+        return;
+    }
+
+    auto framerate = av_d2q(fps, 100);
+
+    codec_ctx->width = width;
+    codec_ctx->height = height;
+    codec_ctx->framerate = framerate;
+    codec_ctx->pix_fmt = format;
+    codec_ctx->time_base = av_inv_q(framerate); // Frames per second
+    if (gop_size > 0)
+        codec_ctx->gop_size = gop_size; // Intra frames per x P frames
+    // CodecCtx->bit_rate = config.BitRate;     // average bit_rate
+
+    if (codec->id == AVCodecID::AV_CODEC_ID_H264 || codec->id == AVCodecID::AV_CODEC_ID_HEVC ||
+        codec->id == AVCodecID::AV_CODEC_ID_MPEG4 || codec->id == AVCodecID::AV_CODEC_ID_MPEG2VIDEO)
+    {
+        codec_ctx->thread_count = 1;
+    }
+
+    if (codec->id == AVCodecID::AV_CODEC_ID_MPEG4 || codec->id == AVCodecID::AV_CODEC_ID_MPEG2VIDEO)
+    {
+        codec_ctx->max_b_frames = 1;
+    }
+
+    ret = avcodec_open2(codec_ctx.get(), codec, NULL);
+    if (ret != 0)
+        return;
 }
 
 NtVideoEncoder::~NtVideoEncoder()
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include "WebServer.h"
 #include "NtRtspApp.h"
 #include "NtDummyVideoDevice.h"
+#include <spdlog/spdlog.h>
 
 int main()
 {
-    auto dummy_dev = NtDummyVideoDevice::createNew(DummyVideoDeviceParameters{});
-
-    auto rtsp = new NtRtspApp();
     auto web = new WebServer();
+    auto dummy_dev = NtDummyVideoDevice::createNew(DummyVideoDeviceParameters{});
+    auto rtsp = new NtRtspApp();
     auto device = new CameraDeviceSource();
 
     rtsp->Start();
